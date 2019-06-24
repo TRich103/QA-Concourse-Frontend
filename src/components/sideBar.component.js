0b777c0ac371c884eadf3a2e88dab57094c8e435
JoshuaGomersall <47025499+JoshuaGomersall@users.noreply.github.com>
2019-06-24 17:24:34 +0100

    Side Bar Update
    The User Guide opens in a new tab , home and universal settings changes the URL

--- src/components/sideBar.component.js
+++ src/components/sideBar.component.js
@@ -32,12 +32,13 @@ export default class sideBar extends React.Component {
 		// Pass on our props
 		<Menu>
 			<a className="sidebar_btn" onClick={() => { document.location.href = "/"; }}>Home</a>
-			<a id="HelperGuide" target="_new" className="menu-item" href="https://docs.google.com/document/d/1AXQ9NMtyfb5IkY0sDhafANRjIISliqCThlpj8kq99LA/edit">User Guide</a>
+
+			<a className="sidebar_btn" href="https://docs.google.com/document/d/1AXQ9NMtyfb5IkY0sDhafANRjIISliqCThlpj8kq99LA/edit" target="_blank" > User Guide</a>
+
 			{show_server_logs ? 
-				<a className="sidebar_btn" onClick={() => { document.location.href = "/system_logs"; }}>
-				System Logs
-				</a>
+			<a className="sidebar_btn" onClick={() => { document.location.href = "/system_logs"; }}>System Logs</a>
 				: ""}
+
 			<a className="sidebar_btn" onClick={() => { document.location.href = "/settings"; }}>Universal Settings</a>
 		</Menu>
 		);
