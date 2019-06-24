1a79a802ad6b12ec59a0b3963ebcc9604991ff70
Bibek1 <bibek.gurung1@nhs.net>
2019-06-24 17:34:25 +0100

    fix to typos

--- src/components/expenses-trainee.component.js
+++ src/components/expenses-trainee.component.js
@@ -4,7 +4,7 @@ import '../css/history.css';
 import { authService } from './modules/authService';
 import AccessDenied from './modules/AccessDenied';
 import { Button, ButtonGroup } from 'reactstrap';
-//import close from './icons/close2.svg';
+import close from './icons/close2.svg';
 
 
 export default class TraineeExpenses extends Component {
@@ -67,13 +67,6 @@ export default class TraineeExpenses extends Component {
     }
 
     componentDidMount() {
-        this.props = {
-            match:{
-                params:{
-                    id : "5d0bb39bd2ba63099c621593"
-                }
-            }
-        }
         axios.get('http://' + process.env.REACT_APP_AWS_IP + ':4000/admin/getRecord/' + this.props.match.params.id)
             .then(response => {
                 this.setState({ record: response.data });
@@ -185,7 +178,7 @@ export default class TraineeExpenses extends Component {
                                                             if (window.confirm('Are you sure you wish to delete this expense?'))
                                                             axios.post('http://'+process.env.REACT_APP_AWS_IP+':4000/admin/removeExpenses/'+this.props.match.params.id, {"expenseType": monthly_expenses.type, "amount": monthly_expenses.amount, "location": index}).then(() => window.location.reload()) } }>
                                                             Delete
-                                                            {/* <img src={close}></img> */}
+                                                            <img src={close}></img>
                                         </button></td>
                                                 </tr>
                                             );
@@ -194,7 +187,7 @@ export default class TraineeExpenses extends Component {
                                 </table >
                             </div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                             <ButtonGroup>
-                                    <Button type="submit" id="createExpenseBtn2" style={{ marginBottom:30}} onClick={() => { document.location.href = "/"; }}>⭠ Back</Button>
+                                    <Button type="submit" id="createExpenseBtn" style={{ marginBottom:30}} onClick={() => { document.location.href = "/"; }}>⭠ Back</Button>
                             </ButtonGroup>
 
                         </div>
