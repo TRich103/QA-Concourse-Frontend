c10f2378e66cdde7d2c2802a1d2bc139ad5113b9
JoshuaGomersall <47025499+JoshuaGomersall@users.noreply.github.com>
2019-06-26 13:29:12 +0100

    Updated Side-Bar With Restrictions For Admin/Recruiter/Finance

--- src/components/sideBar.component.js
+++ src/components/sideBar.component.js
@@ -8,6 +8,7 @@ export default class sideBar extends React.Component {
         super(props);
 			this.state = {
 				show_server_logs: true,
+				show_finance: true,
 				currentUser: authService.currentUserValue
 		}
 	}
@@ -22,6 +23,15 @@ componentDidMount(){
 				show_server_logs: false
 			})
 		}
+		if(this.state.currentUser.token.role === 'admin' || this.state.currentUser.token.role === 'finance'){
+			this.setState({
+				show_finance: true,
+			})
+		}else{
+			this.setState({
+				show_finance: false
+			})
+		}
 	}
 	
 	 showSettings (event) {
@@ -29,6 +39,7 @@ componentDidMount(){
 		}
    render () {
 	const {show_server_logs} = this.state;
+	const {show_finance} = this.state;
 	
 	return (
 		// Pass on our props
@@ -38,8 +49,15 @@ componentDidMount(){
 			{show_server_logs ? 
 			<NavLink className="sidebar_btn" to="/users">Users</NavLink>
 				: ""}
+
+
+			{show_finance ?
 			<NavLink to="/csvreport" className="sidebar_btn" >CSV Report</NavLink>
+				: ""}
+			{show_finance ?
 			<NavLink to="/costreport" className="sidebar_btn" >Cost Report</NavLink>
+				: ""}
+			
 			<NavLink className="sidebar_btn" to="/usersettings"> User Settings </NavLink>
 			<br/><br/>
 		</Menu>
